ae32191dda20ba0fd92e4c1489108accc7eeb29c
Skyxypher <ayomahbrian@gmail.com>
2023-10-04 02:42:24 +0300

    add a new file

---
+++ web.js
@@ -0,0 +1,21 @@
+// dashboard.js
+
+// Function to navigate to the home page
+function home_page() {
+    window.location.href = "index.html";
+}
+
+// Function to navigate to the Classroom page
+function Course() {
+    window.location.href = "Course.html";
+}
+
+// Function to navigate to the About page
+function About() {
+    window.location.href = "about.html";
+}
+
+// Function to navigate to the Contact page
+function Contact() {
+    window.location.href = "contact.html";
+}
